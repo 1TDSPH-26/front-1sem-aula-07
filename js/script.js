85054a3d29cc4c0a5ee7ff7c5e8fa5d0f40ead55
Eduardoguelere <guelereduardo@gmail.com>
2026-05-15 09:17:22 -0300

    fieat: criando redrect sethinteval

--- js/script.js
+++ js/script.js
@@ -134,3 +134,49 @@
 // x **= 5;
 // console.log(x);
 
+// function soma(a=0,b=0){
+//     let resultado = a+b;
+//     console.log("Executar algo!!!");
+//     return resultado;
+// }
+
+//Recebendo parâmetros através do operador SPREAD:
+// function soma(...args){
+//     let resultado = 0;
+    
+//     for(let x = 0; x < args.length ; x++){
+//         resultado += args[x];
+//     }
+
+//     console.log("Resultado da operação:", resultado);
+//     return resultado;
+// }
+
+// //  console.log("Executado no escopo principal!");
+
+// soma(2,2,4,2);
+
+ //Arrow-Function
+//  const soma = (...args) => {
+//     let resultado = 0;
+    
+//     for(let x = 0; x < args.length ; x++){
+//         resultado += args[x];
+//     }
+
+//     console.log("Resultado da operação:", resultado);
+//     return resultado;
+// }
+
+// //  console.log("Executado no escopo principal!");
+
+// soma(2,2,4,2);
+
+
+// const valida = () => 2 > 1;
+
+const intevalo = setInterval(() => {
+    window.location.href = "./index.html";
+    clearInterval(intevalo);
+}, 5000);
+
