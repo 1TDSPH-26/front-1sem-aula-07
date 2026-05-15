cb7494703c57e3f5c92ee623020e4d0dcda2a677
felipemotitsukitan <felipetan13@gmail.com>
2026-05-15 09:17:10 -0300

    feat : Criando redirect com setInterval

--- js/script.js
+++ js/script.js
@@ -153,20 +153,27 @@
 //soma(2,2,4,2);
 
 //Arrow-Function
- const soma = (...args) => {
-    let resultado = 0;
-    
-    for(let x = 0; x < args.length ; x++){
-        resultado += args[x];
-    }
+// const soma = (...args) => {
+//     let resultado = 0;
+//    
+//     for(let x = 0; x < args.length ; x++){
+//         resultado += args[x];
+//     }
+//
+//     console.log("Resultado da operação:", resultado);
+//     return resultado;
+// }
+//
+//   console.log("Executado no escopo principal!");
+//
+// soma(2,2,4,2);
+//
+//
+// const valida = () => 2 > 1;
 
-    console.log("Resultado da operação:", resultado);
-    return resultado;
-}
-
-//  console.log("Executado no escopo principal!");
-
-soma(2,2,4,2);
+const intervalo = setInterval( ()=>{
+    window.location.href = "./index.html"
+    clearInterval(intervalo);
+}, 5000);
 
 
-const valida = () => 2 > 1;
